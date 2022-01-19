bed4b7268d5e9c7807242f64cded4ad8c49817f8
KatharineME <katharine.me@icloud.com>
2022-01-19 15:45:02 -0700

    incorporated star call for cdna to genome

--- README.md
+++ README.md
@@ -73,6 +73,8 @@ Specificy a temporary storage directory like `/tmp/example_prefix/`
 
 Marks duplicate alignments in a coordinate sorted file using flags added by `fixmate -m`. Duplicates are defined as aligned reads whose 5 prime coordinates and orientation (forward or reverse) match. For paried-end reads, both primary (not secondary alignments) read alignments must have matching 5 prime coordinates and share orientation. When a duplicate is detected the highest quality duplicate is kept and the others have the duplicate flag set.
 
+![duplicate](stuff/duplicate.png)
+
 The issue of when and whether to remove duplicates is debated. Duplicates as defined above may result from duplicate fragments made during PCR, which skew the results and should be removed. But duplicates may also be from two different fragments aligning to exact same corrdinates, in which case their sequences may differ and they should be kept. Differentiating between these two types of duplicates is the problem.  
 
 Duplicates may be removed before alignment or after. The risk with removing duplicates before alignment is that you are actually removing real signal instead of duplicate fragments. The risk with removing duplicates after alignment is that you may remove fragments with unique sequences that happen to share coordinates with another alignment.
@@ -95,7 +97,11 @@ Threads
 
 ### samtools flagstat
 
-Counts the number of alignments for each samtools flag type and indicates how successful the alignment was. Information on each of the flags is in the [Samtools specification](https://samtools.github.io/hts-specs/SAMv1.pdf).
+Counts the number of alignments for each samtools flag type and interprets the combination of certain flags with the goal of indicating how well the alignment went. Information on each of the flags is in the [Samtools specification](https://samtools.github.io/hts-specs/SAMv1.pdf).
+
+![flags](stuff/flag.png)
+
+In the actual alignment file, the second colum is FLAG. Each alignment has a flag value which is a unique combination of the flags in the table above. The number 77 for example is flags 1+4+8+64. Samtools flagstat deduces the independent flags and creates file wide statistics on them.
 
 When the mapping of a read is ambiguous, it may have multiple mappings. One mapping is considered primary, and all the others have the __secondary__ flag set.
 
--- src/align.jl
+++ src/align.jl
@@ -24,40 +24,49 @@ function align(
 
     if mo == "dna"
 
-        md = "sr"
+        run(
+            pipeline(
+                `minimap2 -ax sr -t $n_jo -K $(me)G -R "@RG\tID:$sa\tSM:$sa" -a $fai $fq1 $fq2`,
 
+                `samtools fixmate --threads $n_jo -m - -`,
+
+                `samtools sort --threads $n_jo`,
+
+                "$ba.tmp",
+            ),
+        )
+
+    ### minimap2 -ax sr -t 10 -K 4G -R "@RG\tID:$test\tSM:$test" /home/jovyan/craft/data/grch/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz.mmi test_dna_4k.R1.fastq.gz test_dna_4k.R2.fastq.gz | samtools fixmate -u -m - - | samtools sort -u -@2 -T ./tmp/test_run_dna_tmp - | samtools markdup -@8 --reference /home/jovyan/craft/data/grch/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz -O BAM - test_dna_final.bam
+
+
+        run(`samtools markdup --threads $n_jo -s $ba.tmp $ba`)
+
+        rm("$ba.tmp")
+
+        run(`samtools index -@ $n_jo $ba`)
+
+        run(
+            pipeline(`samtools flagstat --threads $n_jo $ba`, "$ba.flagstat"),
+        )
 
     elseif mo == "cdna"
 
-        md = "splice -uf"
+        # Directory where STAR will generate its genome indexes 
+        ge = joinpath(dirname(fa), "star_indexes")
+
+        if !ispath(ge)
+
+            mkdir(ge)
+
+            # Generate genome indexes for STAR
+            run(`star --runThreadN $n_jo --runMode genomeGenerate --genomeDir $ge --genomeFastaFiles $fa`)
+
+        end
+
+        # Run STAR
+        run(`star --runThreadN $n_jo --genomeDir $ge --readFilesIn $fq1 $fq2 --readFilesCommand "gzip --decompress --stdout" --outSAMtype BAM SortedByCoordinate --outFileNamePrefix $sa`)
 
     end
-
-    run(
-        pipeline(
-            `minimap2 -ax $md -t $n_jo -K $(me)G -R "@RG\tID:$sa\tSM:$sa" -a $fai $fq1 $fq2`,
-
-            `samtools fixmate --threads $n_jo -m - -`,
-
-            `samtools sort --threads $n_jo`,
-
-            "$ba.tmp",
-        ),
-    )
-
-### minimap2 -ax sr -t 10 -K 4G -R "@RG\tID:$test\tSM:$test" /home/jovyan/craft/data/grch/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz.mmi test_dna_4k.R1.fastq.gz test_dna_4k.R2.fastq.gz | samtools fixmate -u -m - - | samtools sort -u -@2 -T ./tmp/test_run_dna_tmp - | samtools markdup -@8 --reference /home/jovyan/craft/data/grch/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz -O BAM - test_dna_final.bam
-
-
-    run(`samtools markdup --threads $n_jo -s $ba.tmp $ba`)
-
-    rm("$ba.tmp")
-
-    run(`samtools index -@ $n_jo $ba`)
-
-    run(
-        pipeline(`samtools flagstat --threads $n_jo $ba`, "$ba.flagstat"),
-    )
-
 
     return nothing
 
