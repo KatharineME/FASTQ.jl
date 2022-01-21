b150cdb089aea27d143e7c81f30451f7a71811fc
KatharineME <katharine.me@icloud.com>
2022-01-21 15:53:07 -0700

    test working

--- src/test.jl
+++ src/test.jl
@@ -1,6 +1,6 @@
-function test()::Nothing
+function test(pa::String)::Nothing
 
-    println("Testing")
+    println("Testing environment")
 
     for pr in [
         "fastp",
@@ -19,13 +19,40 @@ function test()::Nothing
 
     end
 
-    for pr in [
-        "configManta.py",
-        "configureStrelkaGermlineWorkflow.py",
-        "configureStrelkaSomaticWorkflow.py",
+    ma = "manta-1.6.0.centos6_x86_64"
+
+    st = "strelka-2.9.10.centos6_x86_64"
+
+    for pr in [ma, st]
+
+        if !(pr in readdir(pa))
+
+            println("You dont have the correct version ($pr)")
+
+            return
+
+        end
+
+    end
+    
+    vo = last(split(pa, "/"))
+
+    id = readlines(pipeline(
+                 `docker run --interactive --detach --tty --user root --volume $pa:/home/$vo centos:centos6 bash`,
+               )
+       )
+
+    for sc in [
+        joinpath(ma, "bin", "runMantaWorkflowDemo.py"),
+        joinpath(st, "bin", "runStrelkaGermlineWorkflowDemo.bash"),
+        joinpath(st, "bin", "runStrelkaSomaticWorkflowDemo.bash"),
     ]
 
-        run(`bash -c "source activate py2 && which $pr"`)
+        re =  readlines(pipeline(`docker exec --interactive $id bash -c "./home/$vo/$(sc)"`))
+
+        println()
+
+        println(join(re, " "))
 
     end
 
