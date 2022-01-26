3762f8e47a1e2c7d3a8d35a8fd718d927ee0f31d
KatharineME <katharine.me@icloud.com>
2022-01-26 10:29:33 -0700

    tweaked trim

--- src/trim.jl
+++ src/trim.jl
@@ -12,17 +12,29 @@ function trim(
     n_jo::Int,
 )::Nothing
 
-    println("Trimming...")
+    if ispath(pa)
 
-    mkpath(pa)
+        println("Skipping trimming because directory already exists: $pa")
 
-    re = joinpath(pa, "fastp.html")
-    
-    ou1 = joinpath(pa, basename(fq1))
+        return nothing
 
-    ou2 = joinpath(pa, basename(fq2))
+    else
+        
+        mkpath(pa)
 
-    run(`fastp --detect_adapter_for_pe --html $re --in1 $fq1 --in2 $fq2 --out1 $ou1 --out2 $ou2`)
+        println("Trimming...")
+
+        ht = joinpath(pa, "fastp.html")
+
+        js = joinpath(pa, "fastp.json")
+        
+        ou1 = joinpath(pa, basename(fq1))
+
+        ou2 = joinpath(pa, basename(fq2))
+
+        run(`fastp --detect_adapter_for_pe --json $js --html $ht --in1 $fq1 --in2 $fq2 --out1 $ou1 --out2 $ou2`)
+
+    end
 
     return nothing
 
