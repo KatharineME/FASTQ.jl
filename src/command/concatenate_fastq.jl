ad71fd749bb9d965c3580a7a154b282d1326a439
KatharineME <katharine.me@icloud.com>
2022-05-14 19:16:38 -0600

    setting.json

--- src/command/concatenate_fastq.jl
+++ src/command/concatenate_fastq.jl
@@ -2,10 +2,6 @@ function concatenate_fastq(se)
 
     fe_va = Fastq.command.read_setting(se)
 
-    pa = joinpath(fe_va["output_directory"], "concatenate_fastq")
-
-    Fastq.support.error_if_directory(pa)
-
     re_ = Fastq.fastq.find(fe_va["dna_read_directory"])
 
     Fastq.fastq.concatenate(re_, fe_va["read_name_scheme"])
