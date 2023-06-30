6769d24ffdfa437488a79a76131fdc123c8acf6a
KatharineME <katharine.me@icloud.com>
2023-06-30 02:02:53 -0600

    moved data, making tests

--- test/Command.jl
+++ test/Command.jl
@@ -24,13 +24,15 @@ TO = joinpath(dirname(@__DIR__), "tool")
 
 n_jo = 8
 
-me = 8G
+me = 8
 
 ge = joinpath(
     DA,
     "ReferenceGenome",
     "GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz",
 )
+
+tr = joinpath(DA, "ReferenceTranscriptome", "Homo_sapiens.GRCh38.cdna.all.fa.gz")
 
 chs = joinpath(DAC, "chromosome.bed.gz")
 
@@ -40,6 +42,18 @@ cd = joinpath(DA, "cDNA"),
 sn = joinpath(TO, "snpEff", "snpEff.jar")
 
 va = joinpath(DA, "Ensembl", "homo_sapiens-chr1_y.vcf.gz")
+
+fr = 51
+
+sd = 0.05
+
+or = "human"
+
+sa = "test"
+
+rs = true
+
+mg = joinpath(DA, "Mouse", "mouse_transcript_mouse_gene.tsv")
 
 # ---- #
 
@@ -59,37 +73,42 @@ FASTQ.Command.call_variants_on_bulk_cdna(
     va,
 )
 
-# FASTQ.Command.measure_gene_expression_of_bulk_cdna(
-#     TE,
-#     cdna_read_directory,
-#     number_of_jobs,
-#     reference_transcriptome,
-#     fragment_length,
-#     fragment_length_standard_deviation,
-#     organism,
-#     mouse_transcript_to_mouse_gene,
-# )
-# 
+# ---- #
+
+FASTQ.Command.measure_gene_expression_of_bulk_cdna(TE, cd, n_jo, tr, fr, sd, or, mg)
+
+# ---- #
+
 # FASTQ.Command.measure_gene_expression_of_single_cell_cdna()
 # 
-# FASTQ.Command.call_variants_on_germline_dna(
-#     output_directory,
-#     read1,
-#     read2,
-#     number_of_jobs,
-#     memory,
-#     sample,
-#     reference_genome,
-#     chromosome_position,
-#     chromosome_name,
-#     snpeff,
-#     molecule,
-#     exome,
-#     tool_directory,
-#     annotate_with_rsid,
-#     variant_database,
-# )
-# 
+# ---- #
+
+DAD = joinpath(DA, "Test", "DNA")
+
+r1 = joinpath(DAD, "test_dna_4k.R1.fastq.gz")
+
+r1 = joinpath(DAD, "test_dna_4k.R2.fastq.gz")
+
+mo = "dna"
+
+FASTQ.Command.call_variants_on_germline_dna(
+    TE,
+    r1,
+    r2,
+    n_jo,
+    me,
+    sa,
+    ge,
+    chs,
+    chn,
+    sn,
+    molecule,
+    exome,
+    tool_directory,
+    annotate_with_rsid,
+    variant_database,
+)
+
 # FASTQ.Command.call_variants_on_somatic_dna(
 #     output_directory,
 #     read1,
