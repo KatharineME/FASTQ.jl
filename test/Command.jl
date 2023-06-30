591b6739d363cf6cf432ae894e00978a3133a8db
KatharineME <katharine.me@icloud.com>
2023-06-30 00:11:54 -0600

    moved data, making tests

--- test/Command.jl
+++ test/Command.jl
@@ -12,116 +12,113 @@ end
 
 # ---- #
 
-DA = joinpath(FASTQ.DA, "Test")
+DA = FASTQ.DA
 
-@test readdir(DA) ==
-      ["c2.all.v7.1.symbols.gmt", "gene_x_statistic_x_number.tsv", "h.all.v7.1.symbols.gmt"]
+DAT = joinpath(FASTQ.DA, "Test")
+
+DAC = joinpath(DAT, "ReferenceGenome", "Chromosome")
+
+TO = joinpath(dirname(@__DIR__), "tool")
+
+# ---- #
+
+n_jo = 8
+
+me = 8G
+
+ge = joinpath(
+    DA,
+    "ReferenceGenome",
+    "GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz",
+)
+
+chs = joinpath(DAC, "chromosome.bed.gz")
+
+chn = joinpath(DAC, "chrn_n.tsv")
+
+cd = joinpath(DA, "cDNA"),
+sn = joinpath(TO, "snpEff", "snpEff.jar")
+
+va = joinpath(DA, "Ensembl", "homo_sapiens-chr1_y.vcf.gz")
 
 # ---- #
 
-sc_ = [-2.0, -1, 0, 0, 1, 2]
-
-n = length(sc_)
-
-# ---- #
-output_directory = TE
-read1 =
-    read2 =
-        somatic_read1,
-        somatic_read2,
-        number_of_jobs,
-        memory,
-        sample,
-        reference_genome,
-        chromosome_position,
-        chromosome_name,
-        snpeff,
-        molecule,
-        exome,
-        tool_directory,
-        annotate_with_rsid,
-        variant_database,
-        cdna_read_directory =
-            reference_transcriptome = fragment_length,
-            fragment_length_standard_deviation,
-            organism,
-            mouse_transcript_to_mouse_gene,
-            FASTQ.Command.call_variants_on_bulk_cdna(
-                output_directory,
-                cdna_read_directory,
-                number_of_jobs,
-                reference_genome,
-                molecule,
-                exome,
-                chromosome_position,
-                chromosome_name,
-                memory,
-                tool_directory,
-                snpeff,
-                annotate_with_rsid,
-                variant_database,
-            )
-
-FASTQ.Command.measure_gene_expression_of_bulk_cdna(
-    output_directory,
-    cdna_read_directory,
-    number_of_jobs,
-    reference_transcriptome,
-    fragment_length,
-    fragment_length_standard_deviation,
-    organism,
-    mouse_transcript_to_mouse_gene,
+FASTQ.Command.call_variants_on_bulk_cdna(
+    TE,
+    cd,
+    n_jo,
+    ge,
+    "dna",
+    false,
+    chs,
+    chn,
+    me,
+    TO,
+    sn,
+    true,
+    va,
 )
 
-FASTQ.Command.measure_gene_expression_of_single_cell_cdna()
-
-FASTQ.Command.call_variants_on_germline_dna(
-    output_directory,
-    read1,
-    read2,
-    number_of_jobs,
-    memory,
-    sample,
-    reference_genome,
-    chromosome_position,
-    chromosome_name,
-    snpeff,
-    molecule,
-    exome,
-    tool_directory,
-    annotate_with_rsid,
-    variant_database,
-)
-
-FASTQ.Command.call_variants_on_somatic_dna(
-    output_directory,
-    read1,
-    read2,
-    somatic_read1,
-    somatic_read2,
-    number_of_jobs,
-    memory,
-    sample,
-    reference_genome,
-    chromosome_position,
-    chromosome_name,
-    snpeff,
-    molecule,
-    exome,
-    tool_directory,
-    annotate_with_rsid,
-    variant_database,
-)
-
-FASTQ.Command.benchmark(
-    output_directory,
-    reference_genome,
-    rtg_tools,
-    number_of_jobs,
-    name_chromosome,
-    query_vcf,
-    truth_vcf,
-    confident_regions_bed,
-)
-
-FASTQ.Command.concatenate_fastq(dna_read_directory, read_name_scheme)
+# FASTQ.Command.measure_gene_expression_of_bulk_cdna(
+#     TE,
+#     cdna_read_directory,
+#     number_of_jobs,
+#     reference_transcriptome,
+#     fragment_length,
+#     fragment_length_standard_deviation,
+#     organism,
+#     mouse_transcript_to_mouse_gene,
+# )
+# 
+# FASTQ.Command.measure_gene_expression_of_single_cell_cdna()
+# 
+# FASTQ.Command.call_variants_on_germline_dna(
+#     output_directory,
+#     read1,
+#     read2,
+#     number_of_jobs,
+#     memory,
+#     sample,
+#     reference_genome,
+#     chromosome_position,
+#     chromosome_name,
+#     snpeff,
+#     molecule,
+#     exome,
+#     tool_directory,
+#     annotate_with_rsid,
+#     variant_database,
+# )
+# 
+# FASTQ.Command.call_variants_on_somatic_dna(
+#     output_directory,
+#     read1,
+#     read2,
+#     somatic_read1,
+#     somatic_read2,
+#     number_of_jobs,
+#     memory,
+#     sample,
+#     reference_genome,
+#     chromosome_position,
+#     chromosome_name,
+#     snpeff,
+#     molecule,
+#     exome,
+#     tool_directory,
+#     annotate_with_rsid,
+#     variant_database,
+# )
+# 
+# FASTQ.Command.benchmark(
+#     output_directory,
+#     reference_genome,
+#     rtg_tools,
+#     number_of_jobs,
+#     name_chromosome,
+#     query_vcf,
+#     truth_vcf,
+#     confident_regions_bed,
+# )
+# 
+# FASTQ.Command.concatenate_fastq(dna_read_directory, read_name_scheme)
