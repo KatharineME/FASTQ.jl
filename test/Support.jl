ad97ca2ebc47d68a6fc068b8743ef35f0885faf3
KatharineME <katharine.me@icloud.com>
2023-08-17 10:39:22 -0600

    tester for index genome files

--- test/Support.jl
+++ test/Support.jl
@@ -20,8 +20,24 @@ FASTQ.Support.trash_remake_directory(DI)
 
 @test isempty(readdir(DI))
 
-readdir(joinpath(homedir(), ".Trash", basename(DI))) == ["Applejack.txt", "RainbowDash.txt"]
+@test readdir(joinpath(homedir(), ".Trash", basename(DI))) == ["Applejack.txt", "RainbowDash.txt"]
 
 # ---- #
-# ---- #
+
+DII = joinpath(FASTQ.TE, "index")
+
+mkdir(DII)
+
+GR = joinpath(FASTQ._DA, "ReferenceGenome", "GRCh38")
+
+GE = joinpath(GR, "GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz")
+
+CHS = joinpath(GR, "Chromosome", "chromosome.bed.gz")
+
+cp(GE, joinpath(DII, basename(GE)))
+
+cp(CHS, joinpath(DII, basename(CHS)))
+
+FASTQ.Support.index_genome_files(joinpath(DII, basename(GE)), joinpath(DII, basename(CHS)))
+
 # ---- #
