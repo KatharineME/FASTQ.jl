586831a26701563dc58dc2212721cf54a3b3d5b0
KatharineME <katharine.me@icloud.com>
2024-01-10 21:47:09 -0700

    happy > Happy

--- test/Benchmark.jl
+++ test/Benchmark.jl
@@ -36,4 +36,4 @@ const N_JO = 8
 
 @test FASTQ.Benchmark.benchmark(TE, GE, RT, N_JO, VQ, VT, BED) === nothing
 
-@test lastindex([fi for fi in readdir(joinpath(TE, "Benchmark", "happy")) if fi != ".DS_Store"]) == 11
+@test lastindex([fi for fi in readdir(joinpath(TE, "Benchmark", "Happy")) if fi != ".DS_Store"]) == 11
