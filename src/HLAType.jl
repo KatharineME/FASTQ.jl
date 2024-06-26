12abd5fa492e35dadcdb555e0294baea16d8fa7a
KatharineME <katharine.me@icloud.com>
2024-06-26 00:11:00 -0600

    hla t tool/

--- src/HLAType.jl
+++ src/HLAType.jl
@@ -2,10 +2,13 @@ module HLAType
 
 using ..FASTQ
 
-function hlatype(ba)
+function hlatype(output_directory, bam, sample, number_of_jobs)
+
+    FASTQ.Support.log_top_level_function()
+
+    FASTQ.Support.error_if_file_missing((reference,))
 
     # copy bam to HLA directory
-
 
     # run xhla
     # docker run -v `pwd`:`pwd` -w `pwd` humanlongevity/hla --sample_id 1020 --input_bam_path tests/1020.bam --output_path test
